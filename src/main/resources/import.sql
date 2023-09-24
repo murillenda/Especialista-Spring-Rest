6c073e61c603179eef0b6d4ee2a460584af7f6db
Murillo Marques <giikthelol@gmail.com>
2023-09-24 00:50:55 -0300

    feat(Repository) ISS-011: Add new Restaurante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+insert into restaurante (nome, taxa_frete) values ('Potiguar', 8.50)
+insert into restaurante (nome, taxa_frete) values ('Complexo Gastronomico', 12)
