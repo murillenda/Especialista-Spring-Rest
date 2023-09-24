48a921dbd059633d920c4af455c50841f990d7d7
Murillo Marques <giikthelol@gmail.com>
2023-09-24 15:56:42 -0300

    feat(Cidade) ISS-014: Add entity and repository cidade

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafood.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cidade {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Estado estado;
+
+}
