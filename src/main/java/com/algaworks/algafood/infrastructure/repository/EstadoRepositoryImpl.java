b6fadf12dd41989c7423297ddad211ebbbf43270
Murillo Marques <giikthelol@gmail.com>
2023-09-24 15:57:17 -0300

    feat(Estado) ISS-014: Add entity and repository estado

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -0,0 +1,42 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class EstadoRepositoryImpl implements EstadoRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Estado> listar() {
+        TypedQuery<Estado> query = manager.createQuery("FROM Estado", Estado.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public Estado buscarPorId(Long id)   {
+        return manager.find(Estado.class, id);
+    }
+
+    @Transactional
+    @Override
+    public Estado salvar(Estado estado) {
+        return manager.merge(estado);
+    }
+
+    @Transactional
+    @Override
+    public void remover(Estado estado) {
+        estado = buscarPorId(estado.getId());
+        manager.remove(estado);
+    }
+}
