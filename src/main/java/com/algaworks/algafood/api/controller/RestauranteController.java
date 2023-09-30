b8c2c3de9719fd83e686da2e62bab6a586e48ade
Murillo Marques <giikthelol@gmail.com>
2023-09-30 12:26:12 -0300

    feat(Restaurante) ISS-027: Implements endpoint atualizarParcial with Patch

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -5,13 +5,17 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -68,5 +72,39 @@ public class RestauranteController {
         } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+
+    // Existem outras formas de resolver o problema do patch sem usar ele, isso é só um exemplo de como utiliza-lo no spring
+    @PatchMapping("/{restauranteId}")
+    public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId,
+                                              @RequestBody Map<String, Object> campos) {
+        Restaurante restauranteAtual = restauranteRepository.buscarPorId(restauranteId);
+
+        if (restauranteAtual == null) {
+            ResponseEntity.notFound().build();
+        }
+
+        merge(campos, restauranteAtual);
+
+        return atualizar(restauranteId, restauranteAtual);
+    }
+
+    private static void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+        // Falando pro objectMapper criar uma instancia do tipo Restaurante usando como base os dadosOrigem
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            // Pedindo pro reflectionUtils buscar um field dessa classe Restaurante com o nome da propriedade passada.
+            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+            field.setAccessible(true);
+
+            // Buscando o valor da propriedade representada pelo field dentro de restauranteOrigem
+            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+            // Coloque nesse campo na classe restauranteDestino o valor da enviado.
+            ReflectionUtils.setField(field, restauranteDestino, novoValor);
+        });
     }
 }
