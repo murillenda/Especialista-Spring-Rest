d2599506cdba8774de6b8d7afa3efb3f139b08ff
Murillo Marques <giikthelol@gmail.com>
2023-10-05 18:55:40 -0300

    feat(Pedido) ISS-043: Add entity Pedido

--- src/main/java/com/algaworks/algafood/domain/model/Endereco.java
+++ src/main/java/com/algaworks/algafood/domain/model/Endereco.java
@@ -9,12 +9,16 @@ public class Endereco {
 
     @Column(name = "endereco_cep")
     private String cep;
+
     @Column(name = "endereco_logradouro")
     private String logradouro;
+
     @Column(name = "endereco_numero")
     private String numero;
+
     @Column(name = "endereco_complemento")
     private String complemento;
+
     @Column(name = "endereco_bairro")
     private String bairro;
 
