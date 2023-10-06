22e8983a1fda87c2da0784f3d0c8d2f49f32568f
Murillo Marques <giikthelol@gmail.com>
2023-10-06 12:06:07 -0300

    feat(exceptions) ISS-045: Errors treatment form one, customizing just error reason and statuscode, no use of catchs

--- src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,13 +1,18 @@
 package com.algaworks.algafood.domain.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class EntidadeNaoEncontradaException extends RuntimeException {
+//@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntidadeNaoEncontradaException extends ResponseStatusException {
 
-    public EntidadeNaoEncontradaException(String message) {
-        super(message);
+    public EntidadeNaoEncontradaException(HttpStatusCode status, String reason) {
+        super(status, reason);
+    }
+
+    public EntidadeNaoEncontradaException(String mensagem) {
+        this(HttpStatus.CONFLICT, mensagem);
     }
 
 }
