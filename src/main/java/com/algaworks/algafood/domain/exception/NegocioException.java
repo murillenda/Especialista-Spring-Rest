7191005d009a7d625465f5e65abb6f331bb94f17
Murillo Marques <giikthelol@gmail.com>
2023-10-07 15:20:11 -0300

    refactor(Exceptions) ISS-050: hierarchy to NegocioException

--- src/main/java/com/algaworks/algafood/domain/exception/NegocioException.java
+++ src/main/java/com/algaworks/algafood/domain/exception/NegocioException.java
@@ -10,4 +10,9 @@ public class NegocioException extends RuntimeException {
         super(mensagem);
     }
 
+    public NegocioException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
+    }
+
+
 }
